84411bc2e4ab8af221e542dcc2356845dd566c8a
Mayank Gangwar <iammayankgangwarbly@gmail.com>
2023-02-12 07:28:55 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Nav from "./component/nav";
 import HomeSlider from "./component/Home-Slider";
 import "./styles.css";
 
 export default function App() {
+  const [subdomain, setSubdomain] = useState(null);
+  useEffect(() => {
+    const host = window.location.host;
+    const arr = host
+      .split(".")
+      .slice(0, host.includes("cz678m-3000.preview.csb") ? -1 : -2);
+    if (arr.length > 0) setSubdomain(arr[0]);
+  }, []);
   return (
     <>
-    <Nav/>
-    <div className="App">
-    <HomeSlider/>
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic ha!</h2>
-    </div>
+      <Nav />
+      <HomeSlider />
+      <div className="App">
+        <h1>{subdomain}</h1>
+        <h2>Start editing to see some magic ha!</h2>
+      </div>
     </>
   );
 }
--- src/component/Home-Slider.jsx
+++ src/component/Home-Slider.jsx
@@ -3,29 +3,31 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 export default function HomeSlider() {
-  var settings = {
-    dots: true,
+  const settings = {
+    dots: false,
     infinite: true,
-    speed: 500,
     slidesToShow: 1,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: true,
   };
   return (
-<div className="container-fluid">
-        <Slider {...settings}>
-          <div>
-            <img src="http://placekitten.com/g/400/200" />
-          </div>
-          <div>
-            <img src="http://placekitten.com/g/400/200" />
-          </div>
-          <div>
-            <img src="http://placekitten.com/g/400/200" />
-          </div>
-          <div>
-            <img src="http://placekitten.com/g/400/200" />
-          </div>
-        </Slider>
-      </div>
+    <div className="container-fluid banner">
+      <Slider {...settings}>
+        <div>
+          <img src="https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" />
+        </div>
+        <div>
+          <img src="https://images.unsplash.com/photo-1502301103665-0b95cc738daf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80" />
+        </div>
+        <div>
+          <img src="https://images.unsplash.com/photo-1544148103-0773bf10d330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" />
+        </div>
+        <div>
+          <img src="https://images.unsplash.com/photo-1471253794676-0f039a6aae9d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" />
+        </div>
+      </Slider>
+    </div>
   );
 }
